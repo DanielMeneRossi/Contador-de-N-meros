86fb024c8db406b28fff5c7d4a10a951a2981b59
Daniel Meneghetti <138326725+DanielMeneRossi@users.noreply.github.com>
2024-05-12 15:06:19 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function contar() {
+	var ini = document.getElementById('inicio')
+	var fim = document.getElementById('fim')
+	var passo = document.getElementById('passos')
+	var res = document.getElementById('res')
+
+	if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+		res.innerHTML = 'Impossível contar'
+	} else {
+		res.innerHTML = 'Contando:<br/>'
+		var i = Number(ini.value)
+		var f = Number(fim.value)
+		var p = Number(passo.value)
+		if(p <= 0) {
+			window.alert('Passo inválido! Considerando PASSO 1')
+			p = 1
+		}
+		if (i < f){
+			for(var c = i; c <= f; c += p){
+				res.innerHTML += `${c} 👍 `
+			}
+			res.innerHTML += `😎`
+		} else {
+			for(var c = i; c >= f; c -= p) {
+				res.innerHTML += `${c} 👍 `
+			}
+			res.innerHTML += `😎`
+		}
+	}
+}
